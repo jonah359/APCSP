ed8e4d1c47e84c0ee33ffff952e16d38ed4b3a85
Jonah DePina <jonahdepina@ORRHS.ORG>
2020-02-24 12:35:09 -0500

    Enter

---
+++ Lesson 3 Variables/app.js
@@ -0,0 +1,19 @@
+// let x = 2
+// let y = 3
+// // your code here
+// y = y + x
+// console.log("The value of y is: " + y + " (y should be 5)");
+// let x = 7;
+// let y = 3;
+// // your code here
+// y = x-y
+// console.log("The value of y is: " + y + " (y should be 4)");
+// let x = 2;
+// let y = 3;
+// // your code here
+// x = x + y + x
+// console.log("The value of x is: " + x + " (x should be 7)");
+// let x = 2;
+// // your code here
+// x = x * x * x
+// console.log("The value of x is: " + x + " (x should be 8)");
